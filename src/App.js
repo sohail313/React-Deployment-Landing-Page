0b2c9bff5e23481449a6a36cd73a3d94b8c7977c
sohail313 <shaikhsohail97692@gmail.com>
2021-04-04 12:21:15 +0530

    Contact Added, Landing Page Completed!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header';
 import Feature from './Components/Feature';
 import About from './Components/About';
 import Presentation from './Components/Presentation';
+import Contact from './Components/Contact';
 import aboutimage from './images/about.png';
 import aboutimage1 from './images/download.png';
 
@@ -14,6 +15,7 @@ function App() {
       <About image={aboutimage} title='Comes With All You Need For Daily Life'  button='Get the app'/>
       <Presentation />
       <About image={aboutimage1} title='Download And Get The App Now'  button='Download'/>
+      <Contact />
     </div>
   );
 }
